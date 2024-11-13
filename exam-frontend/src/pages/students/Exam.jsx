5b36053ef0f7f299f63c6841f0349da63e973c83
chanminswe <chanswe67@gmail.com>
2024-11-13 14:50:23 +0630

    in the progress of Exam Page
    adding questions and ui to display questions

--- exam-frontend/src/pages/students/Exam.jsx
+++ exam-frontend/src/pages/students/Exam.jsx
@@ -5,12 +5,63 @@ const Exam = () => {
   const { examName } = useParams();
   const [loading, setLoading] = useState(false);
 
+  const examData = [
+    {
+      question: "What is React?",
+      answers: [
+        "JavaScript Library",
+        "Java Library",
+        "Tailwind Framework",
+        "CSS Framework",
+      ],
+      correctAnswer: "JavaScript Library",
+    },
+    {
+      question: "Which of the following is a JavaScript runtime environment?",
+      answers: ["React", "Node.js", "Angular", "Vue.js"],
+      correctAnswer: "Node.js",
+    },
+    {
+      question: "What does CSS stand for?",
+      answers: [
+        "Cascading Style Sheets",
+        "Creative Style Sheets",
+        "Cascading Simple Sheets",
+        "Common Style Sheets",
+      ],
+      correctAnswer: "Cascading Style Sheets",
+    },
+    {
+      question: "Which tag is used to define a hyperlink in HTML?",
+      answers: ["<a>", "<link>", "<hyperlink>", "<url>"],
+      correctAnswer: "<a>",
+    },
+    {
+      question:
+        "Which method is used to add an item to an array in JavaScript?",
+      answers: ["push()", "add()", "append()", "insert()"],
+      correctAnswer: "push()",
+    },
+    {
+      question: "What is the correct way to declare a function in JavaScript?",
+      answers: [
+        "function myFunction()",
+        "function = myFunction()",
+        "declare function myFunction()",
+        "myFunction() function",
+      ],
+      correctAnswer: "function myFunction()",
+    },
+  ];
+
   return (
     <div className="text-center font-semibold mx-auto">
       {loading ? (
         <div>Please wait while we are fetching exam data ! </div>
       ) : (
-        <div>{examName} started !</div>
+        <div className="mt-10">
+          <p> {examName} started !</p>
+        </div>
       )}
     </div>
   );
