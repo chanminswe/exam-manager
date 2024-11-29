909f306809a55b587c5df76930785cc968b8d6a9
chanminswe <chanswe67@gmail.com>
2024-11-29 16:59:41 +0630

    will continue for result page and result table soon

--- exam-frontend/src/pages/students/exam-part/Exam.jsx
+++ exam-frontend/src/pages/students/exam-part/Exam.jsx
@@ -26,54 +26,6 @@ const Exam = () => {
     getQuestion();
   }, []);
 
-  // const examData = [
-  //   {
-  //     question: "What is React?",
-  //     answers: [
-  //       "JavaScript Library",
-  //       "Java Library",
-  //       "Tailwind Framework",
-  //       "CSS Framework",
-  //     ],
-  //     correctAnswer: "JavaScript Library",
-  //   },
-  //   {
-  //     question: "Which of the following is a JavaScript runtime environment?",
-  //     answers: ["React", "Node.js", "Angular", "Vue.js"],
-  //     correctAnswer: "Node.js",
-  //   },
-  //   {
-  //     question: "What does CSS stand for?",
-  //     answers: [
-  //       "Cascading Style Sheets",
-  //       "Creative Style Sheets",
-  //       "Cascading Simple Sheets",
-  //       "Common Style Sheets",
-  //     ],
-  //     correctAnswer: "Cascading Style Sheets",
-  //   },
-  //   {
-  //     question: "Which tag is used to define a hyperlink in HTML?",
-  //     answers: ["<a>", "<link>", "<hyperlink>", "<url>"],
-  //     correctAnswer: "<a>",
-  //   },
-  //   {
-  //     question:
-  //       "Which method is used to add an item to an array in JavaScript?",
-  //     answers: ["push()", "add()", "append()", "insert()"],
-  //     correctAnswer: "push()",
-  //   },
-  //   {
-  //     question: "What is the correct way to declare a function in JavaScript?",
-  //     answers: [
-  //       "function myFunction()",
-  //       "function = myFunction()",
-  //       "declare function myFunction()",
-  //       "myFunction() function",
-  //     ],
-  //     correctAnswer: "function myFunction()",
-  //   },
-  // ];
 
   const lengthData = examData.length;
 
