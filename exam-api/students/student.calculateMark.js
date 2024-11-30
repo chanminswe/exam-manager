0badc025e93913acc5179e22f6ab5cd3b4457fae
chanminswe <chanswe67@gmail.com>
2024-11-30 19:46:49 +0630

    editing calculate pages so far

--- exam-api/students/student.calculateMark.js
+++ exam-api/students/student.calculateMark.js
@@ -1,4 +1,6 @@
 const Results = require("../models/exams/Results");
+const Students = require("../models/roles/Users");
+const Exams = require("../models/exams/Exams");
 
 /* 
 This page is basically to calcualte marks
@@ -15,12 +17,49 @@ const calculateMarks = async (req, res) => {
     let marks = 0;
     const { examId } = req.body;
 
-    //get the student Id from the user token/cookie first
-    const { studentId } = req.user;
+    if (!examId) {
+      return res.status(400).json({ message: "Exam Id Unavailable" });
+    }
 
-    console.log(examId, studentId);
+    //get the student Id from the user token/cookie first
+    const studentId = req.user._id;
+
+    if (!studentId) {
+      return res
+        .status(400)
+        .json({ message: "Student Id unavailable / or token expired!" });
+    }
+
+    // to get student Name :)
+    const getStudent = await Students.findById(studentId);
+
+    if (!getStudent) {
+      return res.status(400).json({ message: "Something went wrong fetching student Id !" });
+    }
+
+    const getExam = await Exams.findById(examId);
+
+    if (!getExam) {
+      return res.status(400).json({ message: "Something went wrong fetching Exam Id !" });
+    }
+
+    const createResult = await Results.create({
+      studentId,
+      studentName : getStudent.username,
+      examId,
+      examName : getExam.examName,
+      marks 
+    })
+
+    
+
+    // console.log(examId, studentId);
+
+    return res.status(200).json({ message: "Calculate mark success" });
   } catch (error) {
     console.log("Error Occured at calculate marks !", error);
     return res.status(500).json({ message: "Internal Server Error" });
   }
 };
+
+module.exports = calculateMarks;
